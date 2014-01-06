00f1f69032b95498ae93f986380d277ed743a0cd
Carlos Paliari <carlos@paliari.com.br>
2014-01-06 19:00:38 -0200

    inicial comit

---
+++ date_plugin.js
@@ -0,0 +1,9 @@
+$('#sandbox-container .input-append.date').datepicker({
+    format: "dd/mm/yyyy",
+    todayBtn: "linked",
+    language: "pt-BR",
+    orientation: "bottom auto",
+    calendarWeeks: true,
+    autoclose: true,
+    todayHighlight: true
+})
