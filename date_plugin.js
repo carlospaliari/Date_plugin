63754cab4ea1c38fcc35b15577f3d122053bf8fc
Carlos Paliari <carlos@paliari.com.br>
2014-01-13 09:38:38 -0200

    new_date

--- date_plugin.js
+++ date_plugin.js
@@ -1,41 +1,21 @@
-$('.datepicker').datepicker()
-
-$(document).off('.datepicker.data-api');
-
-$('.datepicker').datepicker({
-    format: 'mm/dd/yyyy',
-    startDate: '-3d'
-})
-
-$('.datepicker').datepicker({
-    startDate: '-3d'
-})
-
-$.fn.datepicker.defaults.format = "mm/dd/yyyy";
-$('.datepicker').datepicker({
-    startDate: '-3d'
-})
-
-$.fn.datepicker.noConflict
-var datepicker = $.fn.datepicker.noConflict();
-$.fn.bootstrapDP = datepicker;
-
-$('.datepicker').datepicker('method', arg1, arg2);
-
-$('.datepicker').datepicker('update');
-$('.datepicker').datepicker('update', '2011-03-05');
-$('.datepicker').datepicker('update', new Date(2011, 2, 5));
-
-$.fn.datepicker.dates['en'] = {
-    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    today: "Today",
-    clear: "Clear"
-};
-
-$('.datepicker').datepicker({
-    language: 'XX'
+('#sandbox-container .input-append.date').datepicker({
+    format: "dd/mm/yyyy",
+    todayBtn: "linked",
+    language: "pt-BR",
+    autoclose: true,
+    todayHighlight: true,
+    beforeShowDay: function (date){
+        if (date.getMonth() == (new Date()).getMonth())
+            switch (date.getDate()){
+                case 4:
+                    return {
+                        tooltip: 'Example tooltip',
+                        classes: 'active'
+                    };
+                case 8:
+                    return false;
+                case 12:
+                    return "green";
+            }
+    }
 });
